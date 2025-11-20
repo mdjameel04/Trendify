9bf00319f492d73c7e66a686458431d1244d06c4
mdjameel04 <jameeltony67@gmail.com>
2025-11-20 23:05:06 +0530

    navbar completed

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -5,10 +5,12 @@ import Image from "next/image";
 import { NavItems } from "@/utils/assets";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import {Menu, X} from "lucide-react"
 
 const Navbar = () => {
   const pathname = usePathname();
 const [menuOpen, setMenuOpen] = useState(false);
+const [active, setActive] = useState("home");
 
 const toggleMenu = () => {
     setMenuOpen(!menuOpen)
@@ -23,7 +25,7 @@ const toggleMenu = () => {
       </div>
 
       {/* menu */}
-      <div className="hidden sm:flex items-center gap-6">
+      <div className="hidden md:flex items-center gap-6">
         {NavItems.map((item) => {
           const isActive = pathname === item.path;
 
@@ -63,15 +65,60 @@ const toggleMenu = () => {
             </Link>
           );
         })}
+   </div>
 
-    {/* mobile menu */}
-      <div className="block md:hidden">
-      <button>
-     {menuOpen ? "" : ""}
+   {/* mobile menu */}
+      <div className="block sm:hidden" onClick={toggleMenu}>
+      <button >
+     {menuOpen ? <X size={24}/> : <Menu size={24}/>}
       </button>
       </div>
-      
+ 
+    <div
+      className={`fixed top-0 right-0 h-full w-64 bg-[#0f0f11] text-white p-6 flex flex-col justify-between 
+      transform transition-transform duration-300
+      ${menuOpen ? "translate-x-0" : "translate-x-full"}
+    `}
+    >
+      {/* Close Button */}
+      <button className="mb-6" onClick={toggleMenu}>
+        <X size={26} className="text-gray-300 hover:text-white" />
+      </button>
+
+      {/* Menu */}
+      <div className="flex flex-col gap-3">
+        {NavItems.map((item) => (
+          <button
+            key={item.name}
+            onClick={() => setActive(item.name)}
+            className={`flex items-center gap-3 px-4 py-3 rounded-xl transition-all
+              ${
+                active === item.name
+                  ? "bg-gradient-to-r from-orange-500 to-orange-600"
+                  : "text-gray-300 hover:bg-[#1a1a1c]"
+              }
+            `}
+          >
+           
+            <span>{item.name}</span>
+          </button>
+        ))}
       </div>
+
+      {/* Bottom Buttons */}
+      <div className="flex flex-col gap-2">
+        
+        <button className="w-full py-2 bg-[#1a1a1c] text-orange-400 rounded-lg">
+          Log In
+        </button>
+        <button className="w-full py-2 bg-orange-500 rounded-lg">
+          Sign Up
+        </button>
+      </div>
+    </div>
+   
+     
+
     </nav>
   );
 };
